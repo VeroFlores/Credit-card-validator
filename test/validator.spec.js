7812485063bbdb836252c4449cf0bcb467c25cf0
VeroFlores <veritocalla@gmail.com>
2020-08-02 15:36:11 -0500

    Se modifica las validaciones ToBe

--- test/validator.spec.js
+++ test/validator.spec.js
@@ -12,16 +12,16 @@ describe('validator', () => {
     });
 
     it('debería retornar true para "4083952015263"', () => {
-      expect(validator.isValid('4083952015263')).toBe('true');
+      expect(validator.isValid('4083952015263')).toBe('tarjeta válida');
     });
   
 
     it('debería retornar true para "79927398713"', () => {
-      expect(validator.isValid('79927398713')).toBe('true');
+      expect(validator.isValid('79927398713')).toBe('tarjeta válida');
     });
 
     it('debería retornar false para "1234567890"', () => {
-      expect(validator.isValid('1234567890')).toBe('false');
+      expect(validator.isValid('1234567890')).toBe('tarjeta inválida');
     });
   });
 
